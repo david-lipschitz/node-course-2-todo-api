22a25fe9059ac34163ae5c3a1e5904dac9a04455
david-lipschitz <david@mypowerstation.biz>
2018-09-12 17:32:02 +0200

    Test the DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -104,3 +104,47 @@ describe('GET /todos/:id', () => {
             .end(done);
     });
 });
+
+describe('DELETE /todos/:id', () => {
+    it('should remove a todo', (done) => {
+        var hexId = todos[1]._id.toHexString(); //get the second item
+
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(200) //assertions / expectations
+            .expect((res) => {
+                expect(res.body.todo._id).toBe(hexId);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err); //error can be rendered by Mocha
+                }
+
+                // query database using findById and make sure it doesn't exist; use toNotExist
+                // expect(x).toNotExist();
+
+                Todo.findById(hexId).then((todo) => {
+                    expect(todo).toNotExist;
+                    done();
+                }).catch((e) => done(e));
+
+            });
+    });
+
+    it('should return 404 if todo not found', (done) => {
+        // make sure you get a 404 back
+        var hexId = new ObjectID().toHexString;
+        request(app)
+            .delete(`/todos/${hexId}`)
+            .expect(404) //remember that expect is an assertion
+            .end(done);
+
+    });
+
+    it('should return 404 if object id is invalid', (done) => {
+        request(app)
+            .delete('/todos/123')
+            .expect(404)
+            .end(done);
+    });
+});
