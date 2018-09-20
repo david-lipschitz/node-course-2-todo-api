627bfbeac3a630628c02dea7914375e5184e3406
david-lipschitz <david@mypowerstation.biz>
2018-09-20 20:56:07 +0200

    Add MLAB as Heroku Config Var

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -18,16 +18,10 @@ mongoose.Promise = global.Promise;
 //     localhostDB = 'mongodb://localhost:27017/TodoAppTest';
 // }
 
-///var config = require('./configmongoose.json'); //and this is not checked into GIT!!
-///var envConfig = config;
-///Object.keys(envConfig).forEach((key) => {
-///    process.env[key] = envConfig[key];
-///});
-
 // new mLab
 let db = {
     localhost: process.env.MONGODB_URI,
-    mlab: 'mongodb://davidl:#G2KgR.7kDFsKgyJ@ds249092.mlab.com:49092/mpst-mongodb-sandbox-todo-api' ///process.env.MLAB
+    mlab: process.env.MONGODB_URI
 };
 //mongoose.connect(db.localhost || db.mlab, {
 
